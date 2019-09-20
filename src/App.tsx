48baae1951298ea0661178e85325a6932cb85631
Nayuta <ttxmtt@163.com>
2019-09-20 23:17:03 +0800

    change history mode

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import Home from './pages/home/Home';
 import { LocaleProvider } from 'antd';
 import { Provider } from 'mobx-react';
 import React from 'react';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { syncHistoryWithStore } from 'mobx-react-router';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 
-const browserHistory = createBrowserHistory();
-const history = syncHistoryWithStore(browserHistory, allStores.routingStore);
+const hashHistory = createHashHistory();
+const history = syncHistoryWithStore(hashHistory, allStores.routingStore);
 
 const App: React.FC = () => {
   return (
