25fd00f07e5a8dfd69d5ababd12a9b516c211eb2
Leo Zhang <leo.jt.zhang@kpmg.com>
2019-09-26 11:34:18 +0800

    add okex api key

---
+++ src/apis/okex/config.ts
@@ -0,0 +1,5 @@
+export const httpKey = 'c396679e-9493-45dc-9c77-2cb76aced87f';
+export const httpSecret = '9A4CE668F12F0DCB2CB5E27CCE1FDC35';
+export const passphrase = 'ttxmTT0804';
+export const urlHost =  'https://www.okex.com';
+// export const websocekHost ='wss://real.okex.com:10442/ws/v3';
