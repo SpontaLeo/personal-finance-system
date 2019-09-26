c524ff560cc876c5c71ec95ce30a716372e78fa6
Leo Zhang <leo.jt.zhang@kpmg.com>
2019-09-26 11:40:06 +0800

    update okex config

--- src/apis/okex/config.ts
+++ src/apis/okex/config.ts
@@ -1,5 +1,5 @@
-export const httpKey = 'c396679e-9493-45dc-9c77-2cb76aced87f';
-export const httpSecret = '9A4CE668F12F0DCB2CB5E27CCE1FDC35';
-export const passphrase = 'ttxmTT0804';
+export const httpKey = '265e07ff-0c39-46fd-85f0-0201fcef4498';
+export const httpSecret = 'CA600358421CA556B00586AAF6A37C03';
+export const passphrase = '8e4c3b2678db03dc2d13d5cd5426b8e3';
 export const urlHost =  'https://www.okex.com';
 // export const websocekHost ='wss://real.okex.com:10442/ws/v3';
