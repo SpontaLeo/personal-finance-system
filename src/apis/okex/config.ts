851f455ef96cbeb5657502687e971190c60a7884
Nayuta <ttxmtt@163.com>
2020-02-02 21:38:44 +0800

    dellete useless code

--- src/apis/okex/config.ts
+++
@@ -1,5 +0,0 @@
-export const httpKey = '265e07ff-0c39-46fd-85f0-0201fcef4498';
-export const httpSecret = 'CA600358421CA556B00586AAF6A37C03';
-export const passphrase = '8e4c3b2678db03dc2d13d5cd5426b8e3';
-export const urlHost =  'https://www.okex.com';
-// export const websocekHost ='wss://real.okex.com:10442/ws/v3';
