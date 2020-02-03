4a1121351d27a146b7f2a93def72b95c6a5c9542
Nayuta <ttxmtt@163.com>
2020-02-03 21:25:07 +0800

    refactor

---
+++ src/components/IconFont/index.ts
@@ -0,0 +1,5 @@
+import { Icon } from 'antd';
+
+export const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1626604_jatc0xo154g.js'
+});
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,52 @@
-import "./Home.scss";
-import { Axis, Chart, Geom, Legend, Tooltip } from "bizcharts";
-import { inject, observer } from "mobx-react";
+import './Home.scss';
 
-import HomeStore from "../../stores/HomeStore";
-import React from "react";
+import { Layout, Menu } from 'antd';
+import { inject, observer } from 'mobx-react';
+
+import HomeStore from '../../stores/HomeStore';
+import { IconFont } from '../../components/IconFont';
+import React from 'react';
+
+const { Sider, Header, Footer } = Layout;
+const MenuItem = Menu.Item;
 
 interface HomeProps {
   homeStore?: HomeStore;
 }
 
-@inject("homeStore")
+@inject('homeStore')
 @observer
 export default class Home extends React.Component<HomeProps> {
   render() {
-    const { data, cols } = this.props.homeStore!;
-
     return (
-      <div className="home">
-        <Chart width={600} height={400} data={data} scale={cols}>
-          <Axis name="genre" title />
-          <Axis name="sold" title />
-          <Legend position="top" />
-          <Tooltip />
-          <Geom type="interval" position="genre*sold" color="genre" />
-        </Chart>
-      </div>
+      <Layout>
+        <Sider
+          style={{
+            height: '100vh',
+            position: 'fixed',
+            left: 0,
+          }}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <MenuItem key="1">
+              <IconFont type="icon-digital-currency" />
+              <span className="nav-text">数字货币</span>
+            </MenuItem>
+            <MenuItem key="2">
+              <IconFont type="icon-fund" />
+              <span className="nav-text">基金定投</span>
+            </MenuItem>
+            <MenuItem key="3">
+              <IconFont type="icon-bill" />
+              <span className="nav-text">个人账单</span>
+            </MenuItem>
+            <MenuItem key="4">
+              <IconFont type="icon-liabilities" />
+              <span className="nav-text">资产负债</span>
+            </MenuItem>
+          </Menu>
+        </Sider>
+      </Layout>
     );
   }
 }
