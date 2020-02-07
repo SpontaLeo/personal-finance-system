ea6bb7d840ea6c77c007100b5a8068985c1cf8c4
Nayuta <ttxmtt@163.com>
2020-02-07 15:48:16 +0800

    build

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,18 @@
-module.exports = function override(config, env) {
-  if (env === 'production') {
-    config.output.publicPath = './';
-  }
-  return config;
-};
+const { override, fixBabelImports } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+
+  process.env === 'production' && process.config.publicPath === './',
+);
+
+// module.exports = function override(config, env) {
+//   if (env === 'production') {
+//     config.output.publicPath = './';
+//   }
+//   return config;
+// };
